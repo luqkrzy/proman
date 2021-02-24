02e34086ce7e4b8f5ad088eb91adbe9d3058f0ec
KKandD <filip.brzezinski102@gmail.com>
2021-02-24 22:51:25 +0100

    rounded flex

--- ProMan/board/views.py
+++ ProMan/board/views.py
@@ -4,6 +4,6 @@ from flask import Blueprint, render_template, url_for, redirect, request, flash,
 board = Blueprint('board', __name__)
 
 
-@board.route("/board", methods=["GET"])
+@board.route("/board", methods=["GET", "POST"])
 def board_page():
     return render_template('board.html')
