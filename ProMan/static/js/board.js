69b1b299a1607b719fefa4b1397abb449f884a4c
Magda7Lena <magdalena7pawlak@gmail.com>
2021-02-27 15:55:19 +0100

    drag and drop

--- ProMan/static/js/board.js
+++ ProMan/static/js/board.js
@@ -37,3 +37,78 @@ document.addEventListener('dblclick', (event) => {
 		}, {once: true})
 	}
 })
+
+initDragAndDrop()
+
+let parentDivOnDragStart;
+
+function initDragAndDrop() {
+    let boards = document.querySelectorAll('.list-group');
+    let cards = document.getElementsByName('card')
+
+    initBoards(boards)
+    initCards(cards)
+}
+
+function initBoards(boards) {
+    for (let board of boards) {
+        board.classList.add('drop-zone')
+        board.addEventListener('drop', onDrop);
+        board.addEventListener('dragover', onDragOver);
+        board.addEventListener('dragleave', onDragLeave);
+    }
+    console.log(boards)
+}
+
+function initCards(cards) {
+    console.log(cards)
+    for (let card of cards) {
+        card.setAttribute('draggable', 'true');
+        card.addEventListener("dragstart", onDragStart);
+        card.addEventListener("dragend", onDragEnd);
+    }
+    console.log(cards)
+}
+
+function onDragStart(event) {
+    this.classList.add('dragged')
+    event.dataTransfer.setData('type/dragged', this);
+    event.dataTransfer.setData('type/dragged-card', this);
+    parentDivOnDragStart = this.parentNode;
+    console.log(this)
+}
+
+function onDragEnd(event) {
+    event.preventDefault();
+    setDropZones(false);
+    this.classList.remove('dragged');
+}
+
+function onDrop(event) {
+    let draggedElement = document.querySelector(".dragged");
+    event.currentTarget.appendChild(draggedElement);
+}
+
+function onDragOver(event) {
+        event.preventDefault();
+}
+
+function onDragLeave(event) {
+    if (event.dataTransfer.types.includes(`type/dragged-card`) &&
+        event.relatedTarget !== null &&
+        event.currentTarget !== event.relatedTarget.closest('.drop-zone')) {
+        this.classList.remove("over-zone");
+        this.classList.add("active-zone");
+    }
+}
+
+function setDropZones(highlighted = true) {
+    let dropZones = document.querySelectorAll(".drop-zone");
+    for (let dropZone of dropZones) {
+        if (highlighted) {
+            dropZone.classList.add("active-zone");
+        } else {
+            dropZone.classList.remove("active-zone");
+        }
+    }
+}
