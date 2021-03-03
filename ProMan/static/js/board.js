0f0612c2db5b7b64994c988a69a8dc065bfa25bb
Luk Krzy <lukaskrzy@gmail.com>
2021-03-03 20:36:06 +0100

    Marge with generate-all

--- ProMan/static/js/board.js
+++ ProMan/static/js/board.js
@@ -6,6 +6,7 @@ class Cards {
 		this.newItemField = document.querySelectorAll('.new-item');
 		this.allCardsContainer = document.querySelector('#allColumnsContainer');
 		this.modalConfirmDeleteBtn = document.querySelector('#modalConfirmDelete');
+		this.addNewColumn = document.getElementById('add-new-card-button');
 	}
 
 	static cardItemMenu = `<span class="dropdown-toggle" data-mdb-toggle="dropdown" id="dropDownCardItem-1"></span>
@@ -177,76 +178,75 @@ class Cards {
 		}
 	}
 
-        addNewItemToCard2() {
+	addNewItemToCard2() {
 
-        document.addEventListener('keydown', (event) => {
-            const target = event.target;
-            if (event.key === 'Enter') {
-                let column = target.parentNode.parentNode
-                let cardDiv = column.querySelector(".cardBody")
-                let columnId = cardDiv.getAttribute('name')
-                let addCard = document.createElement('div')
-                addCard.className = ('edit rounded-3 list-group-item list-group-item-action d-flex justify-content-between mb-1')
-                addCard.setAttribute('edit', 'true')
-                addCard.innerText = target.value
-                const path = window.location.pathname;
-                let board_id = path.split('/')[2];
+		document.addEventListener('keydown', (event) => {
+			const target = event.target;
+			if (event.key === 'Enter') {
+				let column = target.parentNode.parentNode
+				let cardDiv = column.querySelector(".cardBody")
+				let columnId = cardDiv.getAttribute('name')
+				let addCard = document.createElement('div')
+				addCard.className = ('edit rounded-3 list-group-item list-group-item-action d-flex justify-content-between mb-1')
+				addCard.setAttribute('edit', 'true')
+				addCard.innerText = target.value
+				const path = window.location.pathname;
+				let board_id = path.split('/')[2];
 
-                easyHandler.postJson('PUT', '/api/add_card', {
-                    'name': target.value, 'owner_id': 1, 'board_id': board_id,
-                    'column_id': columnId,
-                }, (response) => {
-                    if (response == true) {
-                        document.location.href = path;
-                    } else {
-                        document.location.href = path;
-                        alert('Failed')
-                    }
-                })
+				easyHandler.postJson('PUT', '/api/add_card', {
+					'name': target.value, 'owner_id': 1, 'board_id': board_id, 'column_id': columnId,
+				}, (response) => {
+					if (response === true) {
+						document.location.href = path;
+					} else {
+						document.location.href = path;
+						alert('Failed')
+					}
+				})
 
-                cardDiv.appendChild(addCard)
-                target.value = ''
-            }
-        })
-    }
+				cardDiv.appendChild(addCard)
+				target.value = ''
+			}
+		})
+	}
 
 
-    displayItems() {
-        const path = window.location.pathname;
-        let board_id = path.split('/')[2];
-        easyHandler._getData(`/api/get-cols/${board_id}`, (columns) => {
-            if (columns) {
-                showColumns(columns);
-            } else {
-                document.location.href = "/";
-                alert('Failed')
-            }
-        })
+	displayItems() {
+		const path = window.location.pathname;
+		let board_id = path.split('/')[2];
+		easyHandler._getData(`/api/get-cols/${board_id}`, (columns) => {
+			if (columns) {
+				showColumns(columns);
+			} else {
+				document.location.href = "/";
+				alert('Failed')
+			}
+		})
 
-    }
+	}
 
-    add_column() {
-        addNewColumn.addEventListener('click', (event) => {
-            const path = window.location.pathname;
-            let board_id = path.split('/')[2];
-            event.preventDefault();
-            easyHandler.postJson('PUT', '/api/add_column', {
-                'name': 'New Board', 'owner_id': 1, 'board_id': board_id
-            }, (response) => {
-                // if (response == true) {
-                //     document.location.href = path;
-                // } else {
-                //     document.location.href = path;
-                //     alert('Failed')
-                // }
-            })
+	add_column() {
+		this.addNewColumn.addEventListener('click', (event) => {
+			const path = window.location.pathname;
+			let board_id = path.split('/')[2];
+			event.preventDefault();
+			easyHandler._postJson('PUT', '/api/add_column', {
+				'name': 'New Board', 'owner_id': 1, 'board_id': board_id
+			}, (response) => {
+				// if (response == true) {
+				//     document.location.href = path;
+				// } else {
+				//     document.location.href = path;
+				//     alert('Failed')
+				// }
+			})
 
-        })
-    }
+		})
+	}
 
-    edit_column() {
+	edit_column() {
 
-    }
+	}
 
 
 }
@@ -257,34 +257,33 @@ cards.init()
 const new_item = document.getElementById('new-item')
 
 document.addEventListener('keydown', (event) => {
-    const target = event.target;
-    console.log(target)
-    if (event.key === 'Enter') {
+	const target = event.target;
+	console.log(target)
+	if (event.key === 'Enter') {
 
-        let addCard = document.createElement('div')
-        addCard.className = ('edit rounded-3 list-group-item list-group-item-action d-flex justify-content-between mb-1')
-        addCard.setAttribute('edit', 'true')
-        addCard.innerText = target.value
+		let addCard = document.createElement('div')
+		addCard.className = ('edit rounded-3 list-group-item list-group-item-action d-flex justify-content-between mb-1')
+		addCard.setAttribute('edit', 'true')
+		addCard.innerText = target.value
 
-        let event_id = event.target
-        console.log(event_id.id)
-        let sp2 = document.getElementById(event_id.id).parentElement
-        // let sp2 = document.getElementById(sp1).parentElement
-        console.log('halooo')
-        let parentDiv = sp2.parentNode
-        console.log(sp2)
-        console.log(parentDiv)
-        parentDiv.insertBefore(addCard, sp2)
-        target.value = ''
+		let event_id = event.target
+		console.log(event_id.id)
+		let sp2 = document.getElementById(event_id.id).parentElement
+		// let sp2 = document.getElementById(sp1).parentElement
+		console.log('halooo')
+		let parentDiv = sp2.parentNode
+		console.log(sp2)
+		console.log(parentDiv)
+		parentDiv.insertBefore(addCard, sp2)
+		target.value = ''
 
 
-    }
+	}
 })
 
 function showColumns(columns) {
-    for (let i = 0; i < columns.length; i++) {
-        let outerHtml =
-            `
+	for (let i = 0; i < columns.length; i++) {
+		let outerHtml = `
     	<div class="col-md-auto rounded-3 p-1 alert-dark hover-shadow me-3 mb-3" name="${columns[i].id}" "><!-- start card  -->
     		<div class="bg-transparent border-0 list-group-item d-flex justify-content-between fw-bold mb-1">
     			${columns[i].name}<i class="fas fa-ellipsis-h" data-mdb-toggle="dropdown"></i>
@@ -300,41 +299,41 @@ function showColumns(columns) {
     			<input type="text" class='w-100' placeholder=" + new item"></div>
     	</div><!-- end of card  -->`
 
-        let parent = document.getElementById("columns_section")
-        parent.insertAdjacentHTML("beforeend", outerHtml);
+		let parent = document.getElementById("columns_section")
+		parent.insertAdjacentHTML("beforeend", outerHtml);
 
-        easyHandler._getData(`/api/get-cards${columns[i].id}`, (cards) => {
-            if (cards) {
-                showCards(cards);
-            } else {
-                document.location.href = "/";
-                alert('Failed')
-            }
-        })
-    }
+		easyHandler._getData(`/api/get-cards${columns[i].id}`, (cards) => {
+			if (cards) {
+				showCards(cards);
+			} else {
+				document.location.href = "/";
+				alert('Failed')
+			}
+		})
+	}
 }
 
-function showCards(cards){
-    if(cards.length > 0){
-    let cardBody = document.getElementsByClassName('.cardBody')
-        console.log(cardBody)
-    for(let elem of cardBody){
-        let parent = elem.getAttribute('name')
-        for(let i = 0; i < cards.length; i++){
-            if(parent = cards[i].column_id){
-                let addCard = document.createElement('div')
-                addCard.className = ('edit rounded-3 list-group-item list-group-item-action d-flex justify-content-between mb-1')
-                addCard.setAttribute('edit', 'true')
-                addCard.setAttribute('id', parent)
-                addCard.innerText = cards[i].name
+function showCards(cards) {
+	if (cards.length > 0) {
+		let cardBody = document.getElementsByClassName('.cardBody')
+		console.log(cardBody)
+		for (let elem of cardBody) {
+			let parent = elem.getAttribute('name')
+			for (let i = 0; i < cards.length; i++) {
+				if (parent === cards[i].column_id) {
+					let addCard = document.createElement('div')
+					addCard.className = ('edit rounded-3 list-group-item list-group-item-action d-flex justify-content-between mb-1')
+					addCard.setAttribute('edit', 'true')
+					addCard.setAttribute('id', parent)
+					addCard.innerText = cards[i].name
 
-            }
+				}
 
-        }
+			}
 
-    }
+		}
 
 
-    }
+	}
 }
 
